d0c8230884d9f6b9c7d1a910361c9615a6323449
Mazen Ramadan <mazenramadan00@gmail.com>
2022-03-23 20:50:51 +0200

    login URL

--- account/forms.py
+++ account/forms.py
@@ -5,4 +5,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField(wdiget=forms.PasswordInput)
+    password = forms.CharField(wdiget=forms.PasswordInput) # PasswordInput widget is used to make browser treats it as a password input
--- account/views.py
+++ account/views.py
@@ -5,6 +5,14 @@ from .forms import LoginForm
 
 
 def user_login(request):
+    """
+    This is a function for making user login
+    - Check if the request is POST
+    - Instantiate the form with LoginForm(request.POST)
+    - Check whether the form is valid with form.is_valid()
+    - If submited data is valid we authenticate the user against the database using authenticate() method
+    - If the user was successfully authenticated we check whether the user is_active
+    """
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -20,6 +28,6 @@ def user_login(request):
                     return HttpResponse('Disabled account')
             else:
                 return HttpResponse('Invalid login')
-        else:
-            form = LoginForm()
-        return render(request, 'account/login.html', {'form': form})
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
