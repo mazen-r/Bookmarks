7267f9e5fa4dcf7c8338805109aa6cddd3996e19
Mazen Ramadan <mazenramadan00@gmail.com>
2022-03-23 22:20:10 +0200

    user login & logut, dashboard

--- account/forms.py
+++ account/forms.py
@@ -5,4 +5,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField(wdiget=forms.PasswordInput) # PasswordInput widget is used to make browser treats it as a password input
+    password = forms.CharField(widget=forms.PasswordInput) # PasswordInput widget is used to make browser treats it as a password input
